97618ad60c46626fbb44386ee1f70f4b8f8515ae
Abdulellah Alamer <122591461+AbdulellahAlamer@users.noreply.github.com>
2025-10-15 09:09:37 +0300

    TODO 1

--- 5-6-react-styling-fetch-data/src/components/UserCard.jsx
+++ 5-6-react-styling-fetch-data/src/components/UserCard.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
-import { Card, Button } from 'react-bootstrap'
+import React from "react";
+import { Card, Button } from "react-bootstrap";
 
 function UserCard({ user, onUserClick }) {
   return (
     <Card className="user-card">
       <Card.Body>
         <div>
-          <div className="user-avatar">
-            {user.name.charAt(0)}
-          </div>
+          <div className="user-avatar">{user.name.charAt(0)}</div>
         </div>
 
         <Card.Title>{user.name}</Card.Title>
         <Card.Text>
-          <strong>Username:</strong> {user.username}<br />
-          <strong>Email:</strong> {user.email}<br />
+          <strong>Username:</strong> {user.username}
+          <br />
+          <strong>Email:</strong> {user.email}
+          <br />
           <strong>Phone:</strong> {user.phone}
         </Card.Text>
-      {/* Buttotn*/}
-      
+        <Button onClick={() => onUserClick(user)}>View Details</Button>
       </Card.Body>
     </Card>
-  )
+  );
 }
 
-export default UserCard
+export default UserCard;
